de54449b06f422343227bd57e291ecd06879f040
Ankush Gupta <ankush@helloayda.com>
2023-06-06 13:04:39 +0100

    Refactor Activity Timeline

--- src/components/activity-timeline.tsx
+++ src/components/activity-timeline.tsx
@@ -2,6 +2,48 @@ import { ACTIVITY_FEED_ITEMS, EVENT_TYPE_MAP } from "@/helpers/static_data";
 import { Badge, ListGroup, ListGroupItem } from "react-bootstrap";
 import styles from "./activity-timeline.module.scss";
 import { dateOptions, timeOptions } from "@/helpers/const_maps";
+import { ActivityItem } from "@/helpers/helpers_types";
+
+function renderActivity({
+  eventType,
+  timestamp,
+  unread,
+  text,
+}: ActivityItem): JSX.Element {
+  const dateObj = new Date(timestamp);
+
+  // Replace with existing helper functions
+  const dateString = new Intl.DateTimeFormat("en-GB", dateOptions).format(
+    dateObj
+  );
+  // Replace with existing helper functions
+  const timeString = new Intl.DateTimeFormat("en-GB", timeOptions).format(
+    dateObj
+  );
+
+  return (
+    <ListGroup variant="flush" className="border-bottom">
+      <ListGroupItem
+        className={`${styles.activity_timeline_date} text-secondary fw-semibold`}
+      >
+        <div>
+          {dateString}
+          {renderBadge(unread)}
+        </div>
+        <div>{timeString}</div>
+      </ListGroupItem>
+      <ListGroupItem className="activity_timeline d-flex justify-content-top px-0">
+        <i className={`${EVENT_TYPE_MAP[eventType].icon} fs-3 px-3`}></i>
+        <div className={`${styles.activity_timeline_text} py-1`}>
+          <div className="my-1 py-1">
+            <strong>{EVENT_TYPE_MAP[eventType].label}</strong>
+          </div>
+          <div>{text}</div>
+        </div>
+      </ListGroupItem>
+    </ListGroup>
+  );
+}
 
 function renderBadge(unread: boolean) {
   if (unread)
@@ -15,7 +57,10 @@ function renderBadge(unread: boolean) {
 
 const ActivityTimeline = () => {
   return (
-    <div className="col-sm-3 py-2 m-2 mt-0 min-vh-100 rounded bg-white">
+    <div
+      key="activity-timeline"
+      className="col-sm-3 py-2 m-2 mt-0 min-vh-100 rounded bg-white"
+    >
       <div className="d-flex px-3 justify-content-start align-items-center border-bottom">
         <i className="bi-activity fs-3 px-2 text-success"></i>
         <span>
@@ -24,17 +69,6 @@ const ActivityTimeline = () => {
       </div>
       <div className="px-3 ">
         {ACTIVITY_FEED_ITEMS.map((item, idx) => {
-          const dateObj = new Date(item.timestamp);
-          const dateString = new Intl.DateTimeFormat(
-            "en-GB",
-            dateOptions
-          ).format(dateObj);
-
-          const timeString = new Intl.DateTimeFormat(
-            "en-GB",
-            timeOptions
-          ).format(dateObj);
-
           return (
             <>
               {idx === 0 ? (
@@ -44,31 +78,7 @@ const ActivityTimeline = () => {
               ) : (
                 ""
               )}
-
-              <ListGroup variant="flush" className="border-bottom">
-                <ListGroupItem
-                  className={`${styles.activity_timeline_date} text-secondary fw-semibold`}
-                >
-                  <div>
-                    {dateString}
-                    {renderBadge(item.unread)}
-                  </div>
-                  <div>{timeString}</div>
-                </ListGroupItem>
-                <ListGroupItem className="activity_timeline d-flex justify-content-top px-0">
-                  <i
-                    className={`${
-                      EVENT_TYPE_MAP[item.eventType].icon
-                    } fs-3 px-3`}
-                  ></i>
-                  <div className={`${styles.activity_timeline_text} py-1`}>
-                    <div className="my-1 py-1">
-                      <strong>{EVENT_TYPE_MAP[item.eventType].label}</strong>
-                    </div>
-                    <div>{item.text}</div>
-                  </div>
-                </ListGroupItem>
-              </ListGroup>
+              {renderActivity(item)}
             </>
           );
         })}
